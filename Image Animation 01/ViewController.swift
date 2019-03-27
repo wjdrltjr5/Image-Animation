fc44f96585f4eab3ee0a757c0f9ae52903394d4e
dit08 <dit@imac-3.local>
2019-03-27 14:58:30 +0900

    Image animation

--- Image Animation 01/ViewController.swift
+++ Image Animation 01/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var alienImageView: UIImageView!
+    var count = 0
+    @IBAction func updateButtonPressed(_ sender: Any) {
+        //print("button pressde!")
+        count+=1
+        if count == 6 {
+            count = 1
+        }
+        alienImageView.image = UIImage(named: "frame\(count).png")
+        myLabel.text = "frame\(count).png"
+        /*if count < 5{
+        count+=1
+        /alienImageView.image = UIImage(named: "frame\(count).png")
+        myLabel.text = "frame\(count).png"
+        }else{
+            count = 1}*/
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // 초기 이미지 보이기
+        alienImageView.image = UIImage(named:"frame\(count).png")
+        
     }
 
 
